bfc55ebb81bbb58e32d9271c7d441e195dc98b47
Tekin Yavuz <tekiny@gmail.com>
2023-11-09 14:37:10 -0500

    Cloned from LLM-Finetuning-Hub
    https://github.com/georgian-io/LLM-Finetuning-Hub/blob/main/README.md

---
+++ LLAMA/baseline_inference.sh
@@ -0,0 +1,4 @@
+python llama2_baseline_inference.py --task_type classification --prompt_type zero-shot & wait
+python llama2_baseline_inference.py --task_type classification --prompt_type few-shot & wait
+python llama2_baseline_inference.py --task_type summarization --prompt_type zero-shot & wait
+python llama2_baseline_inference.py --task_type summarization --prompt_type few-shot 
